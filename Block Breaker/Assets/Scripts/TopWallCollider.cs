ae42922de90dc7b69c98ad5aaa35b4facd9067a9
Andrej Vyšinský <andrej.vysinsky@gmail.com>
2020-07-05 17:54:23 +0200

    aspect ratio changed to portrait (9:16)

---
+++ Block Breaker/Assets/Scripts/TopWallCollider.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TopWallCollider : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var image = GameObject.FindGameObjectWithTag("MainUI").GetComponent<RectTransform>();
+        var imageSize = image.sizeDelta;
+
+        imageSize.y /= 30;
+
+        Debug.Log($"Image size: {imageSize}");
+
+        var collider = GetComponent<BoxCollider2D>();
+
+        Debug.Log($"Collider size: {collider.size}");
+
+        collider.size = imageSize;
+
+        
+    }
+}
