439a5fa407523252c144e1c500096dc54ddb2ea7
Andrej Vyšinský <andrej.vysinsky@gmail.com>
2020-07-17 20:11:35 +0200

    removed debug messages

--- Block Breaker/Assets/Scripts/TopWallCollider.cs
+++ Block Breaker/Assets/Scripts/TopWallCollider.cs
@@ -10,17 +10,9 @@ public class TopWallCollider : MonoBehaviour
     {
         var image = GameObject.FindGameObjectWithTag("MainUI").GetComponent<RectTransform>();
         var imageSize = image.sizeDelta;
-
         imageSize.y /= 30;
 
-        Debug.Log($"Image size: {imageSize}");
-
         var collider = GetComponent<BoxCollider2D>();
-
-        Debug.Log($"Collider size: {collider.size}");
-
         collider.size = imageSize;
-
-        
     }
 }
