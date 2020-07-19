09efc41d8ca1e5d0052743be1fa536bb2cac8619
Andrej Vyšinský <andrej.vysinsky@gmail.com>
2020-07-19 18:40:47 +0200

    added event listener class

---
+++ Block Breaker/Assets/Scripts/GameEventListeners.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class GameEventListeners : MonoBehaviour
+{
+    public static GameEventListeners Instance { get; private set; }
+
+    public List<GameObject> listeners;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        if (listeners == null)
+        {
+            listeners = new List<GameObject>();
+        }
+
+        GameObject[] gameObjects = GameObject.FindGameObjectsWithTag("Listener");
+
+        listeners.AddRange(gameObjects);
+    }
+
+    public void AddListener(GameObject gameObject)
+    {
+        if (!listeners.Contains(gameObject))
+        {
+            listeners.Add(gameObject);
+        }
+    }
+}
