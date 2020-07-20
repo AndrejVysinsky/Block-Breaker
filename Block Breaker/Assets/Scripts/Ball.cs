e284375a95dde4fba4d1811b04b8cd20626167f7
Andrej Vyšinský <andrej.vysinsky@gmail.com>
2020-07-20 20:21:05 +0200

    added size modifier for ball

--- Block Breaker/Assets/Scripts/Ball.cs
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -8,30 +8,70 @@ public class Ball : MonoBehaviour
     [SerializeField] float velocityX = 3f;
     [SerializeField] float velocityY = 15f;
 
-    private Rigidbody2D myRigidBody2D;
+    private Rigidbody2D rigidBody2D;
+    private SpriteRenderer spriteRenderer;
+    private TrailRenderer trailRenderer;
 
-    private float currentSpeedModifier = 1.0f;
+    private float speedModifier = 1.0f;
+    private float sizeModifier = 1.0f;
 
     private void Awake()
     {
-        //launch ball
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        trailRenderer = GetComponent<TrailRenderer>();
+
+        LaunchBall();
+    }
+
+    private void LaunchBall()
+    {
         velocityX *= UnityEngine.Random.Range(-0.1f, 0.1f);
-        myRigidBody2D = GetComponent<Rigidbody2D>();
-        myRigidBody2D.velocity = new Vector2(velocityX, velocityY);
+        rigidBody2D.velocity = new Vector2(velocityX, velocityY);
     }
 
     public void IncreaseSpeedModifier(float modifier)
     {
-        myRigidBody2D.velocity /= currentSpeedModifier;
-
-        currentSpeedModifier += modifier;
-
-        myRigidBody2D.velocity *= currentSpeedModifier;
-
-        Debug.Log($"Increased speed modifier to: {currentSpeedModifier}");
+        ChangeSpeedModifierBy(modifier);
     }
 
     public void DecreaseSpeedModifier(float modifier)
+    {
+        ChangeSpeedModifierBy(-modifier);
+    }
+
+    private void ChangeSpeedModifierBy(float amount)
+    {
+        rigidBody2D.velocity /= speedModifier;
+
+        speedModifier += amount;
+
+        rigidBody2D.velocity *= speedModifier;
+
+        Debug.Log($"Changed speed modifier by {amount} to {speedModifier}");
+    }
+
+    public void IncreaseSizeModifier(float modifier)
+    {
+        ChangeSizeModifierBy(modifier);
+    }
+
+    public void DecreaseSizeModifier(float modifier)
+    {
+        ChangeSizeModifierBy(-modifier);
+    }
+
+    private void ChangeSizeModifierBy(float amount)
+    {
+        transform.localScale /= sizeModifier;
+
+        sizeModifier += amount;
+
+        transform.localScale *= sizeModifier;
+
+        Debug.Log($"Changed size modifier by {amount} to {sizeModifier}");
+    }
+
     {
         myRigidBody2D.velocity /= currentSpeedModifier;
 
