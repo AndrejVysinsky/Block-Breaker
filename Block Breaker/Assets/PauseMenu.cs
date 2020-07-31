196ecbcc6c04b74242dd53bcbc7c91d6ec7ce5c3
Andrej Vyšinský <andrej.vysinsky@gmail.com>
2020-07-31 12:29:08 +0200

    added pause menu

---
+++ Block Breaker/Assets/PauseMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pauseScreen;
+
+    public void PauseGame()
+    {
+        pauseScreen.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void ResumeGame()
+    {
+        pauseScreen.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void BackToMenu()
+    {
+        Time.timeScale = 1f;
+        Debug.Log("Loading menu...");
+    }
+}
